885600c2b4000a63a2dab4427d9d9781a8614912
Md Jual Hosain <jualhosain@gmail.com>
2020-09-01 07:50:27 +0600

    Updates

---
+++ precache-manifest.933a9be10b22db041df3e64dae88a267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d1a3885ad79aa64d596aef603ece37",
+    "url": "/greetings_with_time/index.html"
+  },
+  {
+    "revision": "67cefedf0f24cb3ccffa",
+    "url": "/greetings_with_time/static/css/main.e0225eb8.chunk.css"
+  },
+  {
+    "revision": "3da62ef0fd321e0eb23e",
+    "url": "/greetings_with_time/static/js/2.090a83cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/greetings_with_time/static/js/2.090a83cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67cefedf0f24cb3ccffa",
+    "url": "/greetings_with_time/static/js/main.5186653f.chunk.js"
+  },
+  {
+    "revision": "88b6e47d9b8338bde787",
+    "url": "/greetings_with_time/static/js/runtime-main.e69e1876.js"
+  }
+]);
